225ef7639088537964b7810d38b63248a9ea6c33
Shruti Chauhan <117182462+077Shruti@users.noreply.github.com>
2026-09-07 14:50:18 +0530

    Time: 469 ms (5.01%), Space: 78.1 MB (15.83%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,21 +1,27 @@
 class Solution {
     public int firstMissingPositive(int[] A) {
-        boolean[] visitor=new boolean[A.length+1];
-        visitor[0]=true;
         for(int i=0;i<A.length;i++){
-           if(A[i]>0 && A[i]<visitor.length){
-               visitor[A[i]]=true;
-           }
-        }
-        
-        for(int i=0;i<visitor.length;i++){
-            if(visitor[i]==false){
-                return i;
+            while (A[i] >= 1 &&
+                   A[i] <= A.length &&
+                   A[A[i] - 1] != A[i]) {
 
+                swap(A[i] - 1, i, A);
+                
             }
+        }
+        for(int i=0;i<A.length;i++){
+            System.out.println(A[i]);
+           if(A[i]!=i+1){
+                return i+1;
+           }
         }
         return A.length+1;
          
         
+    }
+    public void swap(int a,int b,int[] A ){
+        int t=A[a];
+        A[a]=A[b];
+        A[b]=t;
     }
 }
